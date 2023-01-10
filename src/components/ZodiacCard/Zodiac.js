4bb12a37c28f1e4f6cc57082ef5392cc264e20ce
Steven Bryant <stevelad_83@yahoo.com>
2023-01-10 18:10:17 -0500

    images rendering correctly

--- src/components/ZodiacCard/Zodiac.js
+++ src/components/ZodiacCard/Zodiac.js
@@ -3,7 +3,7 @@ import './Zodiac.css';
 export default function Zodiac(props) {
   return (
     <div className="zodiacCard">
-      <img src={`images/${props.image}.png`} />
+      <img src={`images/${props.name}.png`} />
       <h2>{props.name}</h2>
       <h3>{props.dates}</h3>
     </div>
