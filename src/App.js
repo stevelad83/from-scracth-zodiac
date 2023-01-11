5a33decb68d2b259675fead2a3532286375f6da6
Steven Bryant <stevelad_83@yahoo.com>
2023-01-10 20:29:56 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main/Main.js';
 import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
